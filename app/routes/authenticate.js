dab9ca2fe2f8da309da953aa1a884e0cef19a52a
Jamie White <jamie@jgwhite.co.uk>
2015-06-08 19:40:08 +0100

    Add authenticate form action

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
+import Firebase from 'firebase';
+import config from 'untangle/config/environment';
 
 export default Ember.Route.extend({
+  actions: {
+    authenticate() {
+      let firebase = new Firebase(config.firebase);
+      let credentials = {};
+
+      firebase.authWithPassword(credentials, (error, authData) => {
+        if (error) {
+          console.warn(error);
+        } else {
+          console.info(authData);
+          this.transitionTo('authenticated.welcome');
+        }
+      });
+    }
+  }
 });
