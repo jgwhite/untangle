eba0c05cbbc521b413b37998e43900f71f65331d
Jamie White <jamie@jgwhite.co.uk>
2015-06-08 19:49:06 +0100

    ember g initializer firebase

---
+++ app/initializers/firebase.js
@@ -0,0 +1,12 @@
+import config from 'untangle/config/environment';
+import Firebase from 'firebase';
+
+export function initialize(container, application) {
+  let firebase = new Firebase(config.firebase);
+  application.register('service:firebase', firebase, { instantiate: false });
+}
+
+export default {
+  name: 'firebase',
+  initialize: initialize
+};
